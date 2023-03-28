aac65702993cf82e9b6509d2a0997266b1ef1677
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-28 13:43:56 +0200

    implement Simulator

--- src/Simulator/index.ts
+++ src/Simulator/index.ts
@@ -1,3 +1,35 @@
+import path from "path";
+import fs from "fs";
+
+import { IdGenerator } from "../IdGenerator";
+import { Keys } from "../types";
+
 export class Simulator {
-  constructor() {}
+  private _pk: string;
+  public publicKey: string;
+
+  constructor(pathToPk?: string) {
+    if (pathToPk) {
+      this._pk = this.getPKFromPath(pathToPk);
+      this.publicKey = this.getPublicKey();
+    } else {
+      const keys = this.generateNewId();
+      this._pk = keys.privateKey;
+      this.publicKey = keys.publicKey;
+    }
+  }
+
+  private getPKFromPath(pathToPk: string): string {
+    const pk = fs.readFileSync(path.join(pathToPk, "private.key"));
+    return pk.toString();
+  }
+
+  private getPublicKey(): string {
+    const keys = IdGenerator.generateFromPk(this._pk);
+    return keys.publicKey;
+  }
+
+  private generateNewId(): Keys {
+    return IdGenerator.generateId();
+  }
 }
--- src/Simulator/simulator.test.ts
+++ src/Simulator/simulator.test.ts
@@ -1,8 +1,22 @@
 import { Simulator } from ".";
 
+const EXAMPLE_PUBLIC_KEY =
+  "044dbb54df802c49d3a18108b826ae4b22f65824d6f0485139ed46936ac8600032bcb2130c4c60fc87633fa3f0acb1031383ae331b121316baf6fc6204255d363e";
+
 describe("Simulator", () => {
   it("should initialize", () => {
     const simulator = new Simulator();
     expect(simulator).toBeInstanceOf(Simulator);
+  });
+  it("should initialize from private key in file", () => {
+    const simulator = new Simulator("./config");
+
+    expect(simulator.publicKey).toEqual(EXAMPLE_PUBLIC_KEY);
+  });
+  it("should initialize if no private key is provided", () => {
+    const simulator = new Simulator();
+
+    const pubKey = simulator.publicKey;
+    expect(pubKey.length).toEqual(130);
   });
 });
