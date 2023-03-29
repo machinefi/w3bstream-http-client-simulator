4a374e5862c0f8b512ab7df92818c60aed8b8919
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-29 20:45:44 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,26 @@ export type Keys = {
   publicKey: string;
   privateKey: string;
 };
+
+type EventType = "DATA";
+
+interface Header {
+  pub_id: string;
+  pub_token: string;
+  event_type: EventType;
+}
+
+export interface DataPoint {
+  timestamp: number;
+}
+
+export interface Payload {
+  data: DataPoint;
+  public_key: string;
+  signature: string;
+}
+
+export interface Message {
+  header: Header;
+  payload: string; // The Base64 encoded string representing the `Payload` object
+}
