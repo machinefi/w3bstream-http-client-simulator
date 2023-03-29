9841670be6667627297a8679cabc84e10b4edbc7
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2023-03-29 20:45:38 +0200

    update simulator

--- src/Simulator/index.ts
+++ src/Simulator/index.ts
@@ -1,16 +1,63 @@
+import { SimulatorSigner } from "../SimulatorSigner";
+import { DataPointGenerator } from "../DataPointGenerator";
 import { IdGenerator } from "../IdGenerator";
 import { IdFileManager } from "../IdFileManager";
+import { Message, Payload } from "../types";
+
+class NoDataPointGeneratorError extends Error {
+  constructor() {
+    super("No data point generator has been set");
+  }
+}
 
 export class Simulator {
   private _privateKey: string = "";
+  private _dataPointGenerator: DataPointGenerator<any> | undefined;
+
   public publicKey: string = "";
 
-  constructor(pathToPrivateKey?: string) {
+  constructor(private pub_id: string, private pub_token: string) {}
+
+  init(pathToPrivateKey?: string) {
     this.initFromPathOrGenerateNew(pathToPrivateKey ?? "./");
   }
 
-  sign(): string {
-    return this._privateKey;
+  set dataPointGenerator(generator: DataPointGenerator<any>) {
+    this._dataPointGenerator = generator;
+  }
+
+  generateSingleMessage(): Message {
+    const payload = this.generatePayload();
+    const payloadString = JSON.stringify(payload);
+    const payloadBase64 = Buffer.from(payloadString).toString("base64");
+    return {
+      header: {
+        pub_id: this.pub_id,
+        pub_token: this.pub_token,
+        event_type: "DATA",
+      },
+      payload: payloadBase64,
+    };
+  }
+
+  private generatePayload(): Payload {
+    const dataPoint = this.generateDataPoint();
+
+    return {
+      data: dataPoint,
+      public_key: this.publicKey,
+      signature: SimulatorSigner.sign(
+        JSON.stringify(dataPoint),
+        this._privateKey
+      ),
+    };
+  }
+
+  private generateDataPoint() {
+    if (this._dataPointGenerator === undefined) {
+      throw new NoDataPointGeneratorError();
+    }
+    return this._dataPointGenerator.generateDataPoint();
   }
 
   private initFromPathOrGenerateNew(pathToPk: string): void {
--- src/Simulator/simulator.test.ts
+++ src/Simulator/simulator.test.ts
@@ -2,6 +2,8 @@ import path from "path";
 import fs from "fs";
 
 import { Simulator } from ".";
+import { DataPointGenerator } from "../DataPointGenerator";
+import { DataPoint, Payload } from "./../types";
 
 const movePkFile = (oldPath: string, newPath: string): void => {
   const newPKFile = fs.readFileSync(path.join(oldPath, "private.key"));
@@ -20,16 +22,39 @@ const removePkFile = (path: string): void => {
   }
 };
 
+const decodePayload = (payload: string): Payload => {
+  const jsonString = Buffer.from(payload, "base64").toString();
+  return JSON.parse(jsonString);
+};
+
+const extractDataPointFromPayload = (payload: string): Partial<DataPoint> => {
+  const decodedPayload = decodePayload(payload);
+  return decodedPayload.data;
+};
+
+const PUB_ID_1 = "pub_id_1";
+const PUB_ID_2 = "pub_id_2";
+const PUB_TOKEN_1 = "pub_token_1";
+const PUB_TOKEN_2 = "pub_token_2";
+
 let simulator1: Simulator;
 let simulator2: Simulator;
 
 let publicKey1: string;
 let publicKey2: string;
 
+interface TemperatureDataPoint extends DataPoint {
+  temperature: number;
+}
+
+const randomizer = (): number => Math.random() * 100;
+const timestampGenerator = (): number => Date.now();
+
 describe("Simulator", () => {
   describe("Initialization", () => {
     beforeEach(() => {
-      simulator1 = new Simulator();
+      simulator1 = new Simulator(PUB_ID_1, PUB_TOKEN_1);
+      simulator1.init();
       publicKey1 = simulator1.publicKey;
     });
     afterEach(() => {
@@ -42,7 +67,8 @@ describe("Simulator", () => {
       const newPath = "./testing-simulator1/new-path";
       movePkFile("./", newPath);
 
-      simulator2 = new Simulator(newPath);
+      simulator2 = new Simulator(PUB_ID_2, PUB_TOKEN_2);
+      simulator2.init(newPath);
 
       publicKey2 = simulator2.publicKey;
 
@@ -54,7 +80,8 @@ describe("Simulator", () => {
       expect(publicKey1.length).toEqual(130);
     });
     it("should create new id if path to file is wrong", () => {
-      simulator2 = new Simulator("./wrong/path");
+      simulator2 = new Simulator(PUB_ID_2, PUB_TOKEN_2);
+      simulator2.init("./wrong-path");
       publicKey2 = simulator2.publicKey;
 
       expect(publicKey2.length).toEqual(130);
@@ -66,10 +93,114 @@ describe("Simulator", () => {
       expect(privateKey.length).toEqual(64);
     });
     it("should reuse private.key file if one is provided", () => {
-      simulator2 = new Simulator();
+      simulator2 = new Simulator(PUB_ID_2, PUB_TOKEN_2);
+      simulator2.init();
       publicKey2 = simulator2.publicKey;
 
       expect(publicKey1).toEqual(publicKey2);
+    });
+  });
+  describe("Data Generation", () => {
+    beforeEach(() => {
+      simulator1 = new Simulator(PUB_ID_1, PUB_TOKEN_1);
+      simulator1.init();
+    });
+    afterEach(() => {
+      fs.rmSync(path.join("./", "private.key"), { force: true });
+    });
+    it("should set a data generator", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+    });
+    it("should generate a data point", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+
+      const message = simulator1.generateSingleMessage();
+      const dataPoint = extractDataPointFromPayload(
+        message.payload
+      ) as TemperatureDataPoint;
+
+      expect(dataPoint.temperature).toBeGreaterThanOrEqual(0);
+      expect(dataPoint.temperature).toBeLessThanOrEqual(100);
+
+      expect(dataPoint.timestamp).toBeGreaterThanOrEqual(0);
+    });
+    it("should generate two different data points", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+
+      const message1 = simulator1.generateSingleMessage();
+      const dataPoint1 = extractDataPointFromPayload(
+        message1.payload
+      ) as TemperatureDataPoint;
+      const message2 = simulator1.generateSingleMessage();
+      const dataPoint2 = extractDataPointFromPayload(
+        message2.payload
+      ) as TemperatureDataPoint;
+
+      expect(dataPoint1.temperature).not.toEqual(dataPoint2.temperature);
+    });
+    it("should generate a message with header and payload", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+
+      const message = simulator1.generateSingleMessage();
+
+      expect(message.header.pub_id).toEqual(PUB_ID_1);
+      expect(message.header.pub_token).toEqual(PUB_TOKEN_1);
+      expect(message.header.event_type).toEqual("DATA");
+    });
+    it("should generate a payload with a signature", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+
+      const message = simulator1.generateSingleMessage();
+
+      const payload = decodePayload(message.payload);
+
+      expect(payload.signature).toBeDefined();
+    });
+    it("should sing a message with the private key", () => {
+      const dataGenerator = new DataPointGenerator<TemperatureDataPoint>(
+        () => ({
+          temperature: randomizer(),
+          timestamp: timestampGenerator(),
+        })
+      );
+      simulator1.dataPointGenerator = dataGenerator;
+
+      const message = simulator1.generateSingleMessage();
+
+      const payload = decodePayload(message.payload);
+
+      const signature = payload.signature;
+      expect(signature.length).toBeGreaterThan(0);
     });
   });
 });
